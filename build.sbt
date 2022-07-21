e98e4a59c8dd5713ea6fa6a0e73f9c2e2e8a79b0
Pavel Dudkov <pavel.dudkov@icloud.com>
2022-07-21 11:24:17 +0400

    Create app and add read data from url

---
+++ build.sbt
@@ -0,0 +1,15 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.8"
+
+val circe = Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % "0.13.0")
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "scala_app",
+    libraryDependencies ++= circe
+  )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,41 @@
+import io.circe.generic.auto._
+import io.circe.parser.decode
+import io.circe.syntax._
+import scala.io.Source
+
+import scala.language.implicitConversions
+
+
+object Main extends App {
+
+  case class Name(common: String)
+  case class Country(name: Name, capital: List[String], area: Double, region: String)
+  case class CountryDto(name: String, capital: String, area: Double)
+
+  val sourceUrl = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
+
+  def createDto(country: Country): CountryDto = {
+    CountryDto(
+      name = country.name.common,
+      capital = country.capital.head,
+      area = country.area
+    )
+  }
+
+  val dataJson = Source.fromURL(sourceUrl)
+  val decoded = decode[List[Country]](dataJson.mkString)
+
+   val result = decoded match {
+      case Right(value) =>
+        value
+          .filter(_.region == "Africa")
+          .sortBy(_.area)(Ordering[Double].reverse)
+          .slice(0, 10)
+          .map(createDto)
+          .asJson
+          .noSpaces
+      case Left(value) => throw new RuntimeException(s"Parsing problems: $value")
+  }
+
+  println(result)
+}
